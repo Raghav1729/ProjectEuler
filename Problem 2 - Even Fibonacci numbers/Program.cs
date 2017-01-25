52efe5b46c252f1e48d4ef99d3123ae2f4cb710f
Raghav Mishra <raghavmishra1729@outlook.com>
2017-01-25 16:58:17 +0530

    Problem 2 - Even Fibonacci numbers

---
+++ Problem 2 - Even Fibonacci numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1, b = 2, c;
+            int sum = 2;
+            while (b < 4000000)
+            {
+                c = a + b;
+                if (c % 2 == 0)
+                {
+                    sum = sum + c;
+                }
+                a = b;
+                b = c;
+
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
