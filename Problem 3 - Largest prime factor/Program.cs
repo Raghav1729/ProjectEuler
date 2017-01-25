ce43007ad9fc1a7464981412a61569ddff9c7194
Raghav Mishra <raghavmishra1729@outlook.com>
2017-01-25 17:22:20 +0530

    Problem 3 - Largest prime factor

---
+++ Problem 3 - Largest prime factor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long n = 600851475143;
+            int maxPrimeFactor = 1;
+
+            if (n%2 == 0)
+            {
+                maxPrimeFactor = 2;             
+            }
+
+            for (int i = 3; i <= n; i = i+2)
+            {
+                while (n%i == 0)
+                {
+                    if(maxPrimeFactor < i)
+                    {
+                        maxPrimeFactor = i;
+                    }
+                    n = n/i;
+                }
+            }
+            Console.WriteLine("{0}", maxPrimeFactor);
+        }
+    }
+}
