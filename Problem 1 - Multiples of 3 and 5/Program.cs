9ceb2c3520ad51556c2bb15eb3011f527279ce9c
Raghav Mishra <raghavmishra1729@outlook.com>
2017-01-25 16:47:52 +0530

    Problem 1 - Multiples of 3 and 5

---
+++ Problem 1 - Multiples of 3 and 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int sum = 0;
+            List<int> multiple = new List<int>();
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % a == 0)
+                {
+                    sum  = sum + i;
+                }
+                else if (i % b == 0)
+                {
+                   sum  = sum + i;
+                }
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
