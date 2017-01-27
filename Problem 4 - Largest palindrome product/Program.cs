df6a69b51d5e78c2ad952c0a60e0f75da9f4556e
Raghav Mishra <raghavmishra1729@outlook.com>
2017-01-27 18:20:55 +0530

    Problem 4 - Largest palindrome product

---
+++ Problem 4 - Largest palindrome product/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int maxPal = 0, originalInteger, reversedInteger = 0;
+
+            for (int i = 999; i >= 100; i--)
+            {
+                for (int j = 999; j >= i; j--)
+                {
+                    originalInteger = i * j;
+                    if (originalInteger > maxPal)
+                    {
+                        reversedInteger = ReversedNum(originalInteger);
+
+                        if (originalInteger == reversedInteger && originalInteger > maxPal)
+                        {
+                            maxPal = originalInteger;
+                        }
+                    }
+                }
+            }
+             Console.WriteLine("{0}", maxPal);
+        }
+
+        public static int ReversedNum(int originalInteger)
+        {
+            int remainder, reversedInteger = 0;
+            while (originalInteger != 0)
+            {
+                remainder = originalInteger % 10;
+                reversedInteger = reversedInteger * 10 + remainder;
+                originalInteger /= 10;
+            }
+            return reversedInteger;
+
+        }
+    }
+}
