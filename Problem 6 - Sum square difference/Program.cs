77836073358a463a45466cd4b01f920f0c565cb4
Raghav Mishra <raghavmishra1729@outlook.com>
2017-02-01 11:36:54 +0530

    Problem 6 - Sum square difference

---
+++ Problem 6 - Sum square difference/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {           
+            int n = 100;
+            long sum = n * (n+1)/ 2;
+            long squared = (n * (n + 1) * (2 * n + 1)) / 6;
+            long diff = sum*sum - squared;
+            Console.WriteLine("{0}", diff);
+        }
+    }
+}
